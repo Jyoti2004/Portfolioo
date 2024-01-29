240a7bb341e0b88e9f2582b815a8d8a1074e0451
Jyoti2004 <jyotikalra004@gmail.com>
2024-01-29 22:24:43 +0530

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Jyoti</span> I am a Website Designer and a Constant Learner engaged in learning and including new skills in my Arsenal, specialized in Designing of UI/UX and developing a responsible Website. To know me in more details you may contact me at jyotikalra004@gmail.com"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
